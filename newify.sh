ff87ab0de55a74934a8d703c03a86e48c4f746e9
bunnie <bunnie@kosagi.com>
2021-10-29 01:42:25 +0800

    add a reminder to myself on how not to damage spi flash

--- newify.sh
+++ newify.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+read -p "Did you remember to kill the factory-firmware.py process and scriptminder? (y/N) " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+    
 echo "power on"
 sudo ./bsim.sh 1
 sleep 0.5
@@ -12,6 +19,8 @@ cd ../..
 
 echo "erasing EC flash"
 sudo fomu-flash/fomu-flash -w precursors/blank.bin
+
+sleep 2
 
 echo "power off"
 sudo betrusted-scripts/vbus.sh 0
