7d738902f062738d6c88d7e623717ff92145697e
bunnie <bunnie@kosagi.com>
2021-10-28 23:56:26 +0800

    fix CI script to pull from latest stable; and update precursors

--- precursors/update_ci.sh
+++ precursors/update_ci.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -e
 
-wget https://ci.betrusted.io/latest-ci/loader.bin -O loader.bin
+echo "Updating to latest stabilized release"
+wget https://ci.betrusted.io/releases/LATEST -O /tmp/LATEST
+REV=`cat /tmp/LATEST`
+REVISION="releases/${REV}"
 
-wget https://ci.betrusted.io/latest-ci/xous.img -O xous.img
+wget https://ci.betrusted.io/$REVISION/loader.bin -O loader.bin
 
-wget https://ci.betrusted.io/latest-ci/soc_csr.bin -O soc_csr.bin
+wget https://ci.betrusted.io/$REVISION/xous.img -O xous.img
 
-wget https://ci.betrusted.io/latest-ci/ec_fw.bin -O ec_fw.bin
-wget https://ci.betrusted.io/latest-ci/bt-ec.bin -O bt-ec.bin
-wget https://ci.betrusted.io/latest-ci/wfm_wf200_C0.sec -O wfm_wf200_C0.sec
-wget https://ci.betrusted.io/latest-ci/wf200_fw.bin -O wf200_fw.bin
+wget https://ci.betrusted.io/$REVISION/soc_csr.bin -O soc_csr.bin
+
+wget https://ci.betrusted.io/$REVISION/ec_fw.bin -O ec_fw.bin
+wget https://ci.betrusted.io/$REVISION/bt-ec.bin -O bt-ec.bin
+wget https://ci.betrusted.io/$REVISION/wfm_wf200_C0.sec -O wfm_wf200_C0.sec
+wget https://ci.betrusted.io/$REVISION/wf200_fw.bin -O wf200_fw.bin
