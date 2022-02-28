2153fdbef8e4ed821846c3931b3c3ef190fa253d
bunnie <bunnie@kosagi.com>
2022-02-28 16:09:27 +0800

    update to xous 0.9.7 precursors

--- precursors/update_ci.sh
+++ precursors/update_ci.sh
@@ -2,17 +2,14 @@
 set -e
 
 echo "Updating to latest stabilized release"
-wget https://ci.betrusted.io/releases/LATEST -O /tmp/LATEST
-REV=`cat /tmp/LATEST`
-REVISION="releases/${REV}"
 
-wget https://ci.betrusted.io/$REVISION/loader.bin -O loader.bin
+wget https://ci.betrusted.io/releases/latest/loader.bin -O loader.bin
 
-wget https://ci.betrusted.io/$REVISION/xous.img -O xous.img
+wget https://ci.betrusted.io/releases/latest/xous.img -O xous.img
 
-wget https://ci.betrusted.io/$REVISION/soc_csr.bin -O soc_csr.bin
+wget https://ci.betrusted.io/releases/latest/soc_csr.bin -O soc_csr.bin
 
-wget https://ci.betrusted.io/$REVISION/ec_fw.bin -O ec_fw.bin
-wget https://ci.betrusted.io/$REVISION/bt-ec.bin -O bt-ec.bin
-wget https://ci.betrusted.io/$REVISION/wfm_wf200_C0.sec -O wfm_wf200_C0.sec
-wget https://ci.betrusted.io/$REVISION/wf200_fw.bin -O wf200_fw.bin
+wget https://ci.betrusted.io/releases/latest/ec_fw.bin -O ec_fw.bin
+wget https://ci.betrusted.io/releases/latest/bt-ec.bin -O bt-ec.bin
+wget https://ci.betrusted.io/releases/latest/wfm_wf200_C0.sec -O wfm_wf200_C0.sec
+wget https://ci.betrusted.io/releases/latest/wf200_fw.bin -O wf200_fw.bin
