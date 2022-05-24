c3f07ae80bfdf55cee4c698f1170b048e2f1bc5b
bunnie <bunnie@kosagi.com>
2022-05-25 02:49:37 +0800

    adjust paths on newify-ci.sh

--- ci/newify-ci.sh
+++ ci/newify-ci.sh
@@ -3,14 +3,21 @@
 echo "THIS SCRIPT ASSUMES YOU KNOW WHAT YOU ARE DOING."
 
 echo "power on"
-sudo ./bsim.sh 1
+sudo ../bsim.sh 1
 sleep 0.5
-sudo betrusted-scripts/vbus.sh 1
+sudo ../betrusted-scripts/vbus.sh 1
+
+# this pre-supposes the device was functional, so we can run this erase script
+sleep 5
+echo "erasing PDDB - assuming device was previously tested/functional"
+../betrusted-scripts/usb_update.py --erase-pddb
+sleep 1
 
 echo "erasing SOC flash"
-cd betrusted-scripts/jtag-tools && ./jtag_gpio.py -f precursors/blank.bin --erase -a 0 --erase-len=0xf80000 -r
+cd ../betrusted-scripts/jtag-tools && ./jtag_gpio.py -f precursors/blank.bin --erase -a 0 --erase-len=0xf80000 -r
 
-cd ../..
+cd ..
+cd ..
 
 echo "erasing EC flash"
 sudo fomu-flash/fomu-flash -w precursors/blank.bin
