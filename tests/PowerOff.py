f9639579463f2f1bc595eff9af2c7d1a1d6c0504
bunnie <bunnie@kosagi.com>
2023-06-07 00:51:56 +0800

    tweaking timing of poweroff to accommodate more stuff going on

--- tests/PowerOff.py
+++ tests/PowerOff.py
@@ -95,7 +95,7 @@ class Test(BaseTest):
         time.sleep(0.5)
         GPIO.output(GPIO_VBUS, 0)
         
-        time.sleep(10.0) # total time required from boot to command acceptance
+        time.sleep(11.0) # total time required from boot to command acceptance
         self.console = fdspawn(ser)        
         self.try_cmd("test wfioff\r", "|TSTR|WFIOFF")
         self.console.close()
